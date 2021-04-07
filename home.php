740c436b270ece447b8899a92da5595cee084b86
TranDacLam <tdlam2105@gmail.com>
2021-04-07 08:26:00 +0700

    #2

--- home.php
+++ home.php
@@ -58,7 +58,7 @@ $source = implode(' - ',$_GET);
 				</a>
 
 				<div class="navbar__icon">
-					<a class="header__hotline" href="tel:(+84) 969 789 589">
+					<a class="header__hotline" data-index="6">
 						<div class="header__hotline-main">
 							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-telephone-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 								<path fill-rule="evenodd" d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z" />
@@ -67,7 +67,7 @@ $source = implode(' - ',$_GET);
 						<span>LIÊN HỆ</span>
 					</a>
 					<div class="navbar__icon-line"></div>
-					<a href="https://www.youtube.com/channel/UCK-IzEAL23BogKdUBeGd0_g" target="_blank"><img src="<?php echo THEME_URL  ?>/assets/images/youtube.png" /></a>
+					<a class="navbar__icon-youtube" href="https://www.youtube.com/channel/UCK-IzEAL23BogKdUBeGd0_g" target="_blank"><img src="<?php echo THEME_URL  ?>/assets/images/youtube.png" /></a>
 					<a href="https://www.facebook.com/Garden-Riverside-Fanpage-Ch%E1%BB%A7-%C4%90%E1%BA%A7u-T%C6%B0-111123637720799" target="_blank"><img src="<?php echo THEME_URL  ?>/assets/images/facebook.png" /></a>
 				</div>
 
