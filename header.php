ed63cfa68330fc629e9d6103c1e471efdb02fa69
Tran Dac Lam <lamtd@fireapps.vn>
2021-04-08 09:42:30 +0700

    add google tag manager

--- header.php
+++ header.php
@@ -20,9 +20,24 @@
     <!-- <link rel="preload" href="<?php echo THEME_URL . '/assets/css/main.js'  ?>" as="script"> -->
 
     <?php wp_head(); ?>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N32MKLM');</script>
+    <!-- End Google Tag Manager -->
+
     <script>
         let THEME_URL = "<?php echo THEME_URL ?>";
     </script>
 </head>
 
 <body <?php body_class(); ?>>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N32MKLM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
