746e0157d8861eb09f4cd3535cc0c93bd426c983
Tran Dac Lam <lamtd@fireapps.vn>
2021-04-13 16:07:03 +0700

    add script

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-
+  <script id="GrabCallMobileCRM-widget-script" src="https://cdn.datatuoi.com/scripts/GrabCallCRM.js?business_id=0275e5db9d3c46c3910ca2be2e717383" type="text/javascript" charset="UTF-8" async></script>
 </body>
 <?php wp_footer(); ?>
 
